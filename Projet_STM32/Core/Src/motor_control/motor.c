d26a5cc58db0e47f912e160a17683810d1982720
FlorianBnGld <bonnetgalandflorian@gmail.com>
2025-12-04 11:05:11 +0100

    Duty cycle selection working

--- Projet_STM32/Core/Src/motor_control/motor.c
+++ Projet_STM32/Core/Src/motor_control/motor.c
@@ -6,7 +6,8 @@
  */
 
 #include "motor_control/motor.h"
-
+#define alpha 0.6
+#define ARR 4250
 
 
 int motor_init(){
@@ -16,6 +17,23 @@ int motor_init(){
 int motor_control(h_shell_t* h_shell, int argc, char** argv)
 {
 	int size;
+
+	//Sécurité
+	if (argc < 2) {
+		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "Erreur: Entrez un rapport (0-100)\r\n");
+		h_shell->drv.transmit(h_shell->print_buffer, size);
+		return -1;
+	}
+	int alpha_input = atoi(argv[1]);
+
+	//Clamping
+	if (alpha_input > 100) alpha_input = 100;
+	if (alpha_input < 0) alpha_input = 0;
+
+
+	uint32_t pulse_ch1 = (alpha_input * ARR) / 100;
+	uint32_t pulse_ch2 = ((100 - alpha_input) * ARR) / 100;
+
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
 
@@ -23,11 +41,14 @@ int motor_control(h_shell_t* h_shell, int argc, char** argv)
 	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
 
 
-	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, ((0.6)*4250));
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pulse_ch1);
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, pulse_ch2);
 
-	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, ((1-0.6)*4250));
-
-	size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "PWM ON\r\n");
+	size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE,
+			"PWM Set: CH1=%d%% (%lu), CH2=%d%% (%lu)\r\n",
+			alpha_input, pulse_ch1, (100-alpha_input), pulse_ch2);
 	h_shell->drv.transmit(h_shell->print_buffer, size);
+
+	return 0;
 }
 
